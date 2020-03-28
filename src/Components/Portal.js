82f8b8422d47d821c8d7cd9c6d7990bc4f177376
David Mayou <davidpmayou@gmail.com>
2020-03-28 11:50:32 -0500

    add emit on button click

--- src/Components/Portal.js
+++ src/Components/Portal.js
@@ -1,21 +1,18 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import './Portal.css';
-
+import { useSelector, useDispatch } from 'react-redux';
 import io from 'socket.io-client';
 
+import './Portal.css';
 import Input from './Input';
 
 function Portal() {
     const uri = 'http://localhost:5000';
     const dispatch = useDispatch();
-    const name = useSelector(state => state.name);
     const showPortal = useSelector( state => state.showPortal );
     const portalRoot = document.getElementById('portal-root');
     const el = document.createElement('div');
     let socket = null;
-
     useEffect(() => {
         if (showPortal) {
             portalRoot.appendChild(el);
@@ -24,22 +21,20 @@ function Portal() {
             };
         }
     });
-    // useEffect(() => {
-    //     socket = io(uri);
-    //     socket.on('reply', (reply) => dispatch({ type: 'ADD_REPLY', payload: reply }));
-    //     socket.on('disconnect', () => dispatch({ type: 'DISCONNECT', payload: null }));
-    //     return function() {
-    //         socket = null;
-    //     };
-    // });
-    const handleClick = () => {
-        console.log('click');
-        // socket.emit('name', name);
+    useEffect(() => {
+        socket = io(uri);
+        socket.on('reply', (reply) => dispatch({ type: 'ADD_REPLY', payload: reply }));
+        socket.on('disconnect', () => dispatch({ type: 'DISCONNECT', payload: null }));
+        return function() {
+            socket = null;
+        };
+    });
+    const emitName = (msg) => {
+        socket.emit('name', msg);
     };
     return ReactDOM.createPortal(
         <div className="Portal">
-            <Input />
-            <button onClick={handleClick}>Talk with ELIZA</button>
+            <Input emitName={emitName}/>
         </div>,
         el,
     );
